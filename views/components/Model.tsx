24ebb8dbd1b772b8a9a8349b283f9e2520db5b3c
Richard Junior <computer.richard0923@gmail.com>
2024-10-21 18:03:25 -0300

    #1.0.3: patched Group module bug

--- views/components/Model.tsx
+++ views/components/Model.tsx
@@ -1,13 +1,10 @@
 import {useEffect} from "react";
 import { useAnimations, useGLTF } from "@react-three/drei";
-import { useRef } from "react";
-import { Group } from "three";
 
 useGLTF.preload("/animated_drone.glb");
 
 
 export default function Model() {
-  const group = useRef<Group>(null);
   const {animations, scene} = useGLTF("/animated_drone.glb");
   const {actions} = useAnimations(animations, scene);
 
@@ -18,7 +15,7 @@ export default function Model() {
   
 
   return (
-    <group ref={group} scale={30} position={-2.5}>
+    <group scale={30} position={-2.5}>
       <primitive object={scene} />
     </group>
   )
